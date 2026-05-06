5afd61f186c619c65c657812e25983e1852d5742
mohammad salah <msalah1999@gmail.com>
2026-05-06 03:23:23 +0300

    ljhfdf

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     private List<InputDevice> devices = new List<InputDevice>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject player;
-    public Image image1, image2, image3;
+    public Image image1, image2, image3, imagem;
     public GameObject Videobject;
     public VideoPlayer clip;
     //public void step1()
@@ -56,13 +56,17 @@ public class GameManager : MonoBehaviour
     }
     public IEnumerator step1()
     {
-        player.transform.position = new Vector3(0.365471601f, 0.295673728f, -0.584015489f);
-        player.transform.rotation = new Quaternion(0, 0.707106829f, 0, 0.707106829f);
+        //player.transform.position = new Vector3(0.365471601f, 0.295673728f, -0.584015489f);
+        //player.transform.rotation = new Quaternion(0, 0.707106829f, 0, 0.707106829f);
         yield return new WaitForSeconds(1);
         AudioManager.Instance.PlayAlone("1");
         image1.GetComponent<CanvasGroup>().DOFade(1, 2);
         yield return new WaitForSeconds(22);
         image1.GetComponent<CanvasGroup>().alpha = 0;
+        AudioManager.Instance.PlayAlone("mq");
+        imagem.GetComponent<CanvasGroup>().DOFade(1, 2);
+        yield return new WaitForSeconds(25);
+        imagem.GetComponent<CanvasGroup>().alpha = 0;
         AudioManager.Instance.PlayAlone("2");
         image2.GetComponent<CanvasGroup>().DOFade(1, 2);
         yield return new WaitForSeconds(32);
